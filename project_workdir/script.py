3e853f48a84cfa21f8c60a8bd365723b7f8348ce
ashwin2rai <ashwin2rai@gmail.com>
2020-02-13 15:52:55 -0500

    Changed filenames added .csv

--- project_workdir/script.py
+++ project_workdir/script.py
@@ -18,12 +18,12 @@ write_tocsv(WikiTable.game_df, fname = 'ps4_wiki_list')
 game_dets =  PricingAndRevs(WikiTable.game_df)
 game_dets.get_all_price_histsandrev().write_dict()
 game_dets.get_prepdf_withreg(days = None, publisher_medval = 18, publisher_lowval = 3, dev_medval = 4, dev_lowval = 1).get_successcol(threshold = -0.0001,verbose=True)
-write_tocsv(game_dets.game_df, fname = 'game_list_notext')
+write_tocsv(game_dets.game_df, fname = 'game_list_notext.csv')
 
 game_dets_withcomments = GetRedditComments(game_dets.game_df.drop(['ReleaseDate_NA','ReleaseDate_JP',
        'ReleaseDate_EU'],axis=1))
 game_dets_withcomments.create_reddit_instance().get_reddit_comments()
-write_tocsv(game_dets_withcomments.game_df, fname = 'game_list_withtext')
+write_tocsv(game_dets_withcomments.game_df, fname = 'game_list_withtext.csv')
 
 game_pred = AutoClassifier(game_dets_withcomments.game_df[['Reviews', 'InitPrice',
        'MappedGenres', 'MappedPublishers', 'MappedDevelopers',
@@ -32,7 +32,7 @@ game_pred = AutoClassifier(game_dets_withcomments.game_df[['Reviews', 'InitPrice
 game_pred.preprocess_block()
 game_pred.shallow_model_fit()
 
-#game_df = pd.read_csv(create_datadir_link(filename='game_list_withtext'), index_col = 0)
+#game_df = pd.read_csv(create_datadir_link(filename='game_list_withtext.csv'), index_col = 0)
 game_df = game_dets_withcomments.game_df
 
 game_success = AutoClassifier(game_df[['Reviews', 'InitPrice',
@@ -43,7 +43,7 @@ feature_set = game_success.preprocess_block(load_preproc = 'preproc.sav')
 succes_prob = game_success.load_model(clf = 'gradbooststep').predict_proba(feature_set)
 
 complete_gamedb(game_df, succes_prob)
-write_tocsv(game_df, fname = 'game_list_withpreds')
+write_tocsv(game_df, fname = 'game_list_withpreds.csv')
 #create_postgres_authdict(user='username', password='3124hash', hostandport='51.22.22.22:5432', dbname ='gamedb')
 #sql_pd_write(game_df, create_posgresurl(), 'investigame')
 
